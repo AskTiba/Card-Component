d1f225718f59868eb07143d44ad15833f82959d7
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-03-11 14:26:50 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,36 +5,42 @@ import luxury from "./assets/images/icon-luxury.svg";
 const App = () => {
   return (
     <article className="">
-      <main className="flex justify-center">
-        <section className="flex-col p-10 mt-24 rounded-l-lg bg-bright-orange ">
+      <main className="flex justify-center mt-24 phone">
+        <section className="flex-col p-10 rounded-l-lg radius bg-bright-orange ">
           <img src={sedan} alt="" className="" />
           <h1 className="my-10 text-[24px] font-[700]">SEDANS</h1>
-          <p className="mb-20">
+          <p className="">
             Choose a sedan for its <br /> affordability and excellent
             <br /> fuel economy. Ideal for <br /> cruising in the city or on
             <br /> your next road trip.
           </p>
-          <button className="text-bright-orange">Learn More</button>
+          <button className="hover:bg-bright-orange text-bright-orange">
+            Learn More
+          </button>
         </section>
-        <section className="flex-col p-10 mt-24 bg-dark-cyan">
+        <section className="flex-col p-10 bg-dark-cyan">
           <img src={suv} alt="" className="" />
           <h1 className="my-10 text-[24px] font-[700]">SUVS</h1>
-          <p className="mb-20">
+          <p className="">
             Take an SUV for its spacious <br /> interior, power, and <br />{" "}
             versatility. Perfect for your <br /> next family vacation and <br />{" "}
             off-road adventures.
           </p>
-          <button className="text-bright-orange">Learn More</button>
+          <button className="hover:bg-dark-cyan text-dark-cyan">
+            Learn More
+          </button>
         </section>
-        <section className="flex-col p-10 mt-24 rounded-r-lg bg-very-dark-cyan ">
+        <section className="flex-col p-10 rounded-r-lg bg-very-dark-cyan radius">
           <img src={luxury} alt="" className="" />
           <h1 className="my-10 text-[24px] font-[700]">SEDANS</h1>
-          <p className="mb-20">
+          <p className="">
             Cruise in the best car brands <br /> without the bloated prices.{" "}
             <br /> Enjoy the enhanced comfort <br /> of a luxury rental and
             arrive <br /> in style.
           </p>
-          <button className="text-bright-orange">Learn More</button>
+          <button className="hover:bg-very-dark-cyan text-very-dark-cyan ">
+            Learn More
+          </button>
         </section>
       </main>
     </article>
